af5f95d8224b61cf9bbe1d61b67f159aa5ec6756
Arad Saidi <arad@saidi.one>
2024-05-20 14:55:21 +0200

    Project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "gradient-top": "#ECF5FF",
+        "gradient-bottom": "#FFFFF",
+        primary: "#2C7EF8",
+        "primary-hover-light": "075CD9",
+        "custom-light-blue": "#EBF3FE",
+        fontFamily: {
+          YekanBakh: ["YekanBakh-Regular"],
+        },
+      },
+    },
+  },
+  // ...rest of your configuration
+};
